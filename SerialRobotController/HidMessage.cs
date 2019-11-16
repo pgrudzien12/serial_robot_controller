f99e949245ccd4376082e615502e1a612945775c
Paweł Grudzień <p.grudzien12@live.com>
2019-11-16 10:31:07 +0100

    actual code

---
+++ SerialRobotController/HidMessage.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Linq;
+using System.IO.Ports;
+using SharpDX.DirectInput;
+
+namespace SerialRobotController
+{
+    public class HidMessage
+    {
+        //
+        // 10000000 10000000 10000000 10000000 10000000 10000000 10000000 10000000
+        // byte0    byte1    byte2    byte3    byte4    byte5    byte6    byte7
+        // Modifier Arrows   ???      ???      Analog0X Analog0Y Analog1X Analog1Y
+        //
+
+        /// <summary>
+        /// 128 = correct message
+        /// 64 = ?
+        /// 32 = ?
+        /// 16 = ?
+        /// 8 = ?
+        /// 4 = alt
+        /// 2 = shift
+        /// 1 = ctrl
+        /// </summary>
+        private byte byte0 = 128;
+
+        /// <summary>
+        /// 128 = ?
+        /// 64 = ?
+        /// 32 = ?
+        /// 16 = ?
+        /// 8 = up
+        /// 4 = down
+        /// 2 = left
+        /// 1 = right
+        /// </summary>
+        private byte byte1 = 128;
+        private byte byte2 = 128;
+        private byte byte3 = 128;
+        private byte byte4 = 128;
+        private byte byte5 = 128;
+        private byte byte6 = 128;
+        private byte byte7 = 128;
+
+        public HidMessage()
+        {
+
+        }
+        public HidMessage(ConsoleKeyInfo key)
+        {
+            SetConsoleKey(key);
+        }
+
+        public void SetConsoleKey(ConsoleKeyInfo key)
+        {
+            byte0 = 128;
+            if (key.Modifiers == ConsoleModifiers.Control)
+            {
+                byte0 |= 1;
+            }
+            if (key.Modifiers == ConsoleModifiers.Shift)
+            {
+                byte0 |= 2;
+            }
+            if (key.Modifiers == ConsoleModifiers.Alt)
+            {
+                byte0 |= 4;
+            }
+
+            /// arrows
+            byte1 = 128;
+            if (key.Key == ConsoleKey.UpArrow)
+            {
+                byte1 |= 8;
+            }
+            if (key.Key == ConsoleKey.DownArrow)
+            {
+                byte1 |= 4;
+            }
+            if (key.Key == ConsoleKey.LeftArrow)
+            {
+                byte1 |= 2;
+            }
+            if (key.Key == ConsoleKey.RightArrow)
+            {
+                byte1 |= 1;
+            }
+        }
+
+        internal void SetKeyboardState(KeyboardState state)
+        {
+            if (state.PressedKeys.Exists(k => k == Key.Right))
+                byte1 |= 1;
+            if (state.PressedKeys.Exists(k => k == Key.Left))
+                byte1 |= 2;
+            if (state.PressedKeys.Exists(k => k == Key.Down))
+                byte1 |= 4;
+            if (state.PressedKeys.Exists(k => k == Key.Up))
+                byte1 |= 8;
+
+            if (state.PressedKeys.Exists(k => k == Key.RightControl))
+                byte0 |= 1;
+            if (state.PressedKeys.Exists(k => k == Key.LeftControl))
+                byte0 |= 1;
+
+            if (state.PressedKeys.Exists(k => k == Key.LeftShift))
+                byte0 |= 2;
+            if (state.PressedKeys.Exists(k => k == Key.RightShift))
+                byte0 |= 2;
+
+            if (state.PressedKeys.Exists(k => k == Key.LeftAlt))
+                byte0 |= 4;
+            if (state.PressedKeys.Exists(k => k == Key.RightAlt))
+                byte0 |= 4;
+        }
+
+        internal void SetJoystickState(JoystickState state)
+        {
+            if(state.X > 45000)
+                byte1 |= 1;
+            if (state.X < 25000)
+                byte1 |= 2;
+            if (state.Y > 45000)
+                byte1 |= 4;
+            if (state.Y < 25000)
+                byte1 |= 8;
+            if (state.Buttons[3])
+                byte0 |= 1;
+            if (state.Buttons[2])
+                byte0 |= 2;
+
+            byte4 += (byte)(state.X / 512);
+            byte5 += (byte)(state.Y / 512);
+
+            byte6 += (byte)(state.RotationX / 512);
+            byte7 += (byte)(state.RotationY / 512);
+
+            //Console.Write("XY:");
+            //Console.Write(Convert.ToString((byte)(state.X / 512), 2).PadLeft(8, '0'));
+            //Console.Write(" ");
+            //Console.Write(Convert.ToString((byte)(state.Y / 512), 2).PadLeft(8, '0'));
+
+            //Console.Write("   Rotation:");
+            //Console.Write(Convert.ToString((byte)(state.RotationX / 512), 2).PadLeft(8, '0'));
+            //Console.Write(" ");
+            //Console.Write(Convert.ToString((byte)(state.RotationY / 512), 2).PadLeft(8, '0'));
+            //Console.WriteLine();
+        }
+
+        public void Send(SerialPort serialPort)
+        {
+            serialPort.Write(new byte[] { byte0, byte1, byte2, byte3, byte4, byte5, byte6, byte7, 0 }, 0, 9);
+        }
+    }
+}
---
+++ SerialRobotController/Program.cs
@@ -0,0 +1,106 @@
+﻿using SharpDX.DirectInput;
+using System;
+using System.IO.Ports;
+
+namespace SerialRobotController
+{
+    internal class Program
+    {
+        private static SerialPort _serialPort;
+
+        private static void Main(string[] args)
+        {
+            var port = SelectComPort(SerialPort.GetPortNames());
+            if (port == null)
+            {
+                Console.WriteLine("No COM port found");
+                return;
+            }
+            var directInput = new DirectInput();
+
+            Guid joystickGuid = GetJoystickGuid(directInput);
+            
+            var keyboard = new Keyboard(directInput);
+            //keyboard.Properties.BufferSize = 128;
+            keyboard.Acquire();
+            var joystick = new Joystick(directInput, joystickGuid);
+            //joystick.Properties.BufferSize = 128;
+            joystick.Acquire();
+
+            _serialPort = new SerialPort();
+
+            _serialPort.PortName = port;
+            _serialPort.BaudRate = 115200;
+            _serialPort.RtsEnable = true;
+
+            _serialPort.Open();
+            DateTime lastKeyStroke = DateTime.MaxValue;
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(50);
+
+                joystick.Poll();
+                var state = joystick.GetCurrentState();
+
+                keyboard.Poll();
+                var keyboardstate = keyboard.GetCurrentState();
+
+                HidMessage message = new HidMessage();
+                message.SetJoystickState(state);
+                message.SetKeyboardState(keyboardstate);
+                message.Send(_serialPort);
+
+                while (_serialPort.BytesToRead > 0)
+                {
+                    int c = _serialPort.ReadChar();
+                    //if (c < 2)
+                    //    Console.Write(c);
+                    //else if (c == 2)
+                    //    Console.Write(' ');
+                    //else if (c == 32)
+                    //    Console.WriteLine();
+                    Console.Write((char)c);
+                }
+            }
+        }
+
+        private static string SelectComPort(string[] comPorts)
+        {
+            if (comPorts.Length == 0)
+                return null;
+            if (comPorts.Length == 1)
+                return comPorts[0];
+
+            for (int i = 0; i < comPorts.Length; i++)
+            {
+                Console.WriteLine($"{i}: {comPorts[i]}");
+            }
+            char c = '\0';
+            while(true)
+            {
+                var key = Console.ReadKey(true);
+
+                Console.WriteLine(key.KeyChar);
+                if (!char.IsDigit(key.KeyChar))
+                    continue;
+
+                int no = int.Parse(key.KeyChar.ToString());
+                if (no >= comPorts.Length)
+                    continue;
+                return comPorts[no];
+            }
+
+            return comPorts[int.Parse(c.ToString())];
+        }
+
+        private static Guid GetJoystickGuid(DirectInput directInput)
+        {
+            // Find a Joystick Guid
+            var joystickGuid = Guid.Empty;
+            foreach (var deviceInstance in directInput.GetDevices(DeviceType.FirstPerson, DeviceEnumerationFlags.AllDevices))
+                joystickGuid = deviceInstance.InstanceGuid;
+            return joystickGuid;
+        }
+    }
+}
